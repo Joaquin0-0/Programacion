d59b9df63b0667f63a07324d2f28f9c48a3d5e13
Jorge-Pomo <jorge.pomo.sgpl@gmail.com>
2020-11-04 09:32:09 +0100

    miprimer commit

---
+++ Boletin_Extra_t3/B12.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Boletin_Extra_t3;
+
+/**
+ * @author DAM
+ *
+ */
+public class B12 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
