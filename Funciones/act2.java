32a48427ea40137c459a248ab52186e34baeaf00
Jorge-Pomo <jorge.pomo.sgpl@gmail.com>
2020-11-30 10:32:23 +0100

    act5

--- Funciones/act2.java
+++ Funciones/act2.java
@@ -5,23 +5,22 @@ import java.util.Scanner;
 public class act2 {
 
 	static Scanner sc = new Scanner(System.in);
-	
+
 	public static int numero(int num) {
-			int resu;
-			
-			resu = num * 2;
-			
+		int resu;
+
+		resu = num * 2;
+
 		return resu;
 	}
-	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		System.out.println("Dime numero:");
 		int num = sc.nextInt();
-		
+
 		int numero = numero(num);
-		
+
 		System.out.println(numero);
 	}
 
--- Funciones/act4.java
+++ Funciones/act4.java
@@ -5,31 +5,31 @@ import java.util.Scanner;
 public class act4 {
 
 	static Scanner sc = new Scanner(System.in);
-	
+
 	public static int maximo(int num1, int num2) {
 		int resu = 0;
-		
-		if (num1 > num2) {
+
+		if(num1 > num2) {
 			resu = num1;
-		}else {
+		}else{
 			resu = num2;
 		}
-		
+
 		return resu;
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		int num1;
 		int num2;
-		
+
 		System.out.println("Dime el primer número: ");
 		num1 = sc.nextInt();
-		
+
 		System.out.println("Dime el segundo número: ");
 		num2 = sc.nextInt();
-		
+
 		int resu = maximo(num1, num2);
 		System.out.println(resu);
 	}
